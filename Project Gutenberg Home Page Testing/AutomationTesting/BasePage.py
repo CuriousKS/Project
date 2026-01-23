cfaf9447bdb05f427b2cba5f34a16e84968c2f04
CuriousKS <keshav__sahu@outlook.com>
2026-01-23 14:13:25 +0530

    added  test_book_download  test function

--- Project Gutenberg Home Page Testing/AutomationTesting/BasePage.py
+++ Project Gutenberg Home Page Testing/AutomationTesting/BasePage.py
@@ -7,24 +7,19 @@ class ProjectGutenberg:
     def __init__(self,driver):
         self.driver=driver
         self.about1=(By.XPATH,"//label[@class='dropdown-button']")
-        self.frequently_downloaded1=(By.XPATH,"//a[@class='link-freq-downloaded']")
-        self.main_categories1=(By.XPATH,"//a[@class='link-main-categories']")
+        self.frequently_downloaded1=(By.XPATH,"/html/body/div[1]/header/div[3]/div[2]/a[1]")
+        self.main_categories1=(By.PARTIAL_LINK_TEXT,"Main Categories")
         self.reading_lists1=(By.XPATH,"//a[@class='link-reading-lists']")
         self.search_options1=(By.XPATH,"//a[@class='link-advanced-search']")
         self.search_bar1=(By.XPATH,"//input[@class='search-input']")
         self.go_button1=(By.XPATH,"//button[@class='search-button']")#
         self.paypal1=(By.XPATH,"//input[@class='donbtn']")
         self.donate1=(By.XPATH,"//a[@class='donate-link']")
-        self.icon1=(By.XPATH,"//img[@alt='Project Gutenberg']")
-        
-        
-        
-#         self.FacebookIcon2=(By.XPATH,"//img[@alt='Facebook icon']") #<--2 web elements
-#         self.MastodonIcon2=(By.XPATH,"//img[@alt='Mastodon icon']") #<--2 web elements
-#         self.BlueskyIcon2=(By.XPATH,"//img[@alt='Bluesky icon']")   #<--2 web elements
-#         
-#         self.nfone2=(By.XPATH,"//li[normalize-space(text())='News feeds of new eBooks:']")  #<--news feed of new ebooks
-        
+        self.icon1=(By.XPATH,"//img[@alt='Project Gutenberg']")        
+#        self.FacebookIcon2=(By.XPATH,"//img[@alt='Facebook icon']") #<--2 web elements
+#        self.MastodonIcon2=(By.XPATH,"//img[@alt='Mastodon icon']") #<--2 web elements
+#        self.BlueskyIcon2=(By.XPATH,"//img[@alt='Bluesky icon']")   #<--2 web elements       
+#        self.nfone2=(By.XPATH,"//li[normalize-space(text())='News feeds of new eBooks:']")  #<--news feed of new ebooks        
         self.apg2=(By.XPATH,"//div[@class='footer']//a[@href='/about/']")   #<--About Project Gutenberg        
         self.PrivacyPolicy2=(By.XPATH,"//a[normalize-space(text()) = 'Privacy policy']")
         self.Permissions2=(By.XPATH,"//a[normalize-space(text()) = 'Permissions']")
@@ -37,8 +32,9 @@ class ProjectGutenberg:
     def get_common_element(self,x):
         temp=getattr(self,x)
         try:
-            return WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(temp))
-        except TimeoutError:
+            temp1 = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(temp))
+            return temp1 
+        except :#TimeoutError:
             return None
             
 
--- Project Gutenberg Home Page Testing/AutomationTesting/BaseTest.py
+++ Project Gutenberg Home Page Testing/AutomationTesting/BaseTest.py
@@ -8,9 +8,17 @@ from selenium.webdriver.support.ui import WebDriverWait
 import requests as rq
 
 class TestLogin:
-    @pytest.fixture(scope="function", autouse=True)
-    def setup_and_teardown(self):
-        self.driver = webdriver.Chrome()
+    @pytest.fixture(params=["chrome","firefox"],scope="function",autouse=True) 
+    def setup_and_teardown(self,request):
+        if request.param=="chrome":
+            self.driver = webdriver.Chrome()
+            
+        if request.param=="firefox":
+            self.driver = webdriver.Firefox()
+
+        if request.param=="edge":
+            self.driver = webdriver.Edge()
+
         self.driver.maximize_window()
         self.driver.get("https://www.gutenberg.org")
         self.page=ProjectGutenberg(self.driver)
@@ -18,31 +26,43 @@ class TestLogin:
         self.driver.quit()
 
 
-    def test_search_bar(self):
+    def test_book_download(self):
         print("\n\n\n\n")
         self.page.search("The Roman hat mystery by Ellery Queen")
         print(self.page.get_title())
-        assert "The Roman hat mystery by Ellery Queen" in  self.page.get_title() ,"ERROR in test_search_bar"
-    
-    
+        assert "The Roman hat mystery by Ellery Queen" in  self.page.get_title() ,"ERROR in test_search_bar while downloading a book"
+        download_url=self.driver.find_element(By.XPATH,"//a[@accesskey='1']").get_attribute('href')
+        response=rq.get(download_url)
+        assert response.status_code==200,"DOWNLOAD FAILED!"
+        assert len(response.content)>0,"DOWNLOAD FILE IS EMPTY!"
+        print(len(response.content))
+        
+       
     def test_visibility_of_common_elements(self):
         print("\n\n\n\n")
         d1=self.page.__dict__
         for i in d1:
             if i=='driver':
                 continue
-            if check.is_true(self.page.get_common_element(i).is_enabled() != False ,i+" not found!") :
-                print(i,"<-----This element is available!")
+            
+            if check.is_true(self.page.get_common_element(i) != None ,i+" not found!") :
+                    print(i,"<-----This element is visible")
+                    if check.is_true(self.page.get_common_element(i).is_enabled() != False ,i+" not enabled!"):
+                        print("\t",i,"<-----This element is enable")
+                    else:
+                       print("\t",i,"<-----THIS ELEMENT IS NOT ENABLE")
+                   
             else:
-                print(i,"<-----THIS ELEMENT IS NOT AVAILABLE!")
+                print(i,"<-----THIS ELEMENT IS NOT VISIBLE!")
+                                    
     
                             
    
-    def test_no_broken_links(self):
+    def test_no_broken_links(self,request):
         print("\n\n\n\n")
         x=self.page.get_all_urls()
         if len(x)==0 :
-            print("No link found :Test Passed!")
+            print("No link found :Test Passed")
             assert True       
         else:
             for i in x:
@@ -55,6 +75,9 @@ class TestLogin:
                 temp = i+"  <---There is some essue with this link"   
                 if check.is_true(status < 400,temp):
                     print(i+"  <---This link PASSED the test")
+                    
+    
+    
                     
                                
             
